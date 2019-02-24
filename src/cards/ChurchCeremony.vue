c385dc97ebf13007e9c35fe5dfe0ec5d235821c6
Ovidiu Suciu <s_o_lazio@yahoo.com>
2019-02-24 10:09:45 +0000

    Minor text updates

--- src/cards/ChurchCeremony.vue
+++ src/cards/ChurchCeremony.vue
@@ -1,45 +1,47 @@
 <template>
-    <v-flex xs12 lg10 offset-lg1 pb-3>
-        <v-card color="accent" class="grey--text text--darken-4">
-                <v-img
-        :src="require('../assets/st-volodmyrs-cathedral.jpg')" alt="st volodmyrs cathedral kiev"
+  <v-flex xs12 lg10 offset-lg1 pb-3>
+    <v-card color="accent" class="grey--text text--darken-4">
+      <v-img
+        :src="require('../assets/st-volodmyrs-cathedral.jpg')"
+        alt="st volodmyrs cathedral kiev"
         aspect-ratio="1.8"
-        ></v-img>
-            <v-card-text>
-            <div>
-                <div class="headline">Church Ceremony</div>
-                <div>
-                <v-icon class="body-2">access_alarm</v-icon>
-                Friday 2:00 PM
-                </div>
-                <div>
-                <v-icon>place</v-icon>
-                <a href="https://www.google.com/maps/place/St.+Volodymyr's+Cathedral/@50.4442142,30.5021347,16.38z/data=!4m5!3m4!1s0x40d4cef7c7731ddd:0x37cc95586b807512!8m2!3d50.4449191!4d30.5087329" class="link-style">Володимирський кафедральний собор, бульвар Тараса Шевченка, 20, Київ</a>
-                </div>
-            </div>
-            </v-card-text>
-            <v-divider light></v-divider>
-            <v-card-text class="text-xs-left">
-                <div>
-                    <div class="subheading">The church wedding ceremony will take place on Friday, July 12th at 2.00 PM at St Volodymyr's Cathedral in Kiev. The attendance is optional, but everyone is welcomed to join us while we talk to God.
-                    </div>
-                </div>
-            </v-card-text>
-            <v-divider light></v-divider>
-            <v-card-actions class="pa-3 text-xs-left">
-            Attendance is optional
-            <v-spacer></v-spacer>
+      ></v-img>
+      <v-card-text>
+        <div>
+          <div class="headline">Church Ceremony</div>
+          <div>
+            <v-icon class="body-2">access_alarm</v-icon>Friday 2:00 PM
+          </div>
+          <div>
+            <v-icon>place</v-icon>
+            <a
+              href="https://www.google.com/maps/place/St.+Volodymyr's+Cathedral/@50.4442142,30.5021347,16.38z/data=!4m5!3m4!1s0x40d4cef7c7731ddd:0x37cc95586b807512!8m2!3d50.4449191!4d30.5087329"
+              class="link-style"
+            >Володимирський кафедральний собор, бульвар Тараса Шевченка, 20, Київ</a>
+          </div>
+        </div>
+      </v-card-text>
+      <v-divider light></v-divider>
+      <v-card-text class="text-xs-left">
+        <div>
+          <div
+            class="subheading"
+          >The church wedding ceremony will take place on Friday, July 12th at 2.00 PM at St Volodymyr's Cathedral in Kiev. The attendance is optional, but everyone is welcome to join us while we talk to God.</div>
+        </div>
+      </v-card-text>
+      <v-divider light></v-divider>
+      <v-card-actions class="pa-3 text-xs-left">Attendance is optional
+        <v-spacer></v-spacer>
 
-            <v-icon class="indigo--text">location_city</v-icon>
-            </v-card-actions>
-        </v-card>
-    </v-flex>
+        <v-icon class="indigo--text">location_city</v-icon>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
 
 <style scoped>
 .link-style {
-
-    text-decoration: none;
+  text-decoration: none;
 }
 
 /* unvisited link */
@@ -54,11 +56,11 @@
 
 /* mouse over link */
 .link-style:hover {
-  color: #F50057;
+  color: #f50057;
 }
 
 /* selected link */
 .link-style:active {
   color: #b46494;
-} 
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import AppHome from '../components/AppHome.vue';
-import AppRegister from '../components/AppRegister.vue';
-import AppInfo from '../components/AppInfo.vue';
-import AppFAQ from '../components/AppFAQ.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import AppHome from "../components/AppHome.vue";
+import AppRegister from "../components/AppRegister.vue";
+import AppInfo from "../components/AppInfo.vue";
+import AppFAQ from "../components/AppFAQ.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-export default new VueRouter ({
-    mode: 'history',
-    routes: [
+export default new VueRouter({
+  routes: [
     {
-        path: '/',
-        name: 'AppHome',
-        component: AppHome
+      path: "/",
+      name: "AppHome",
+      component: AppHome
     },
     {
-        path: '/register',
-        name: 'AppRegister',
-        component: AppRegister
+      path: "/register",
+      name: "AppRegister",
+      component: AppRegister
     },
     {
-        path: '/info',
-        name: 'AppInfo',
-        component: AppInfo
+      path: "/info",
+      name: "AppInfo",
+      component: AppInfo
     },
     {
-        path: '/FAQ',
-        name: 'AppFAQ',
-        component: AppFAQ
-    }    
-    ]
-})
+      path: "/FAQ",
+      name: "AppFAQ",
+      component: AppFAQ
+    }
+  ]
+});
